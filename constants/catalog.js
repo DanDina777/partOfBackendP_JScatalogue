822c6c9b9e1c904df37dcef1c5806065f0629f4e
Dinara <97847584+DanDina777@users.noreply.github.com>
2022-04-17 16:40:48 +0600

    Add files via upload

---
+++ constants/catalog.js
@@ -0,0 +1,25 @@
+const CATALOG = [
+    {
+        id: 'game1',
+        name: 'Root',
+        img: 'img/root_img.png',
+        price: 24000,
+        description: 'ghj',
+        // filters: '',
+        quantity: 0,
+        sold: 0,
+        // comments: ''
+    
+    },
+    {
+        id: 'game2',
+        name: 'Root 2',
+        img: 'img/root_img.png',
+        price: 21000,
+        // description: '',
+        // filters: '',
+        quantity: 0,
+        sold: 0,
+        // comments: ''
+    }
+]
